b244c27f54cc84d972ea0dcc546a4cc77ec2b764
dmytroleskivUA <121440440+dmytroleskivUA@users.noreply.github.com>
2023-05-26 19:37:26 +0000

    Deploying to gh-pages from @ dmytroleskivUA/dmytroleskiv-goit-js-hw-09@0de2c6baf480dad722662bc7358a17fc2c7cfa46 🚀

---
+++ 01-color-switcher.3ebe6153.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;function o(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,n=setInterval(o,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n),n=null}));
+//# sourceMappingURL=01-color-switcher.3ebe6153.js.map
