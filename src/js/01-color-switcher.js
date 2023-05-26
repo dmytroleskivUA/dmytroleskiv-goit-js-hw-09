0de2c6baf480dad722662bc7358a17fc2c7cfa46
dmytroleskivUA <121440440+dmytroleskivUA@users.noreply.github.com>
2023-05-26 20:37:02 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  startBtn.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function onStopBtnClick() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
